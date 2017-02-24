ec68b5d412020fe77c5cb9e459863a0eaab5ab39
Gerardo Montoya <gerardo.montoya@hulu.com>
2017-02-24 12:55:57 -0600

    First commit and mysql export all grants

---
+++ README.md
@@ -0,0 +1,1 @@
+* Collection of useful scripts, links, info or anything to be used, edited or anything in the future.
---
+++ mysql/export_all_grants.sh
@@ -0,0 +1,2 @@
+MYSQL_CONN="-uuser -ppassword"
+mysql ${MYSQL_CONN} --skip-column-names -A -e"SELECT CONCAT('SHOW GRANTS FOR ''',user,'''@''',host,''';') FROM mysql.user WHERE user<>''" | mysql ${MYSQL_CONN} --skip-column-names -A | sed 's/$/;/g' > AllUserGrants.sql
